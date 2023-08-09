f6f3ee1e98a63e882ed445cd929d7e3665b6a16c
bmong4mong0318 <bmong4mong0318@gmail.com>
2023-08-09 17:43:28 +0900

    fix

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-# 출력에 사용될 색상 변수 설정
 YELLOW='\033[1;33m'
 GREEN='\033[1;32m'
 RESET='\033[0m'
 
-# MariaDB 사용 가능 여부를 체크
 i=0
 while ! mysqladmin -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD ping >/dev/null 2>&1; do
   if [ $i -eq 0 ]; then
@@ -18,24 +16,14 @@ while ! mysqladmin -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD ping >/dev/null
 done
 echo "${GREEN}Waiting for MariaDB at Wordpress ... done${RESET}"
 
-# WordPress 코어 다운로드
 wp-cli core download --allow-root
-
-# WordPress 설정 파일 생성
 wp-cli config create --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
-
-# WordPress 설치
 wp-cli core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-# 구독자 사용자 생성
 wp-cli user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=subscriber --allow-root
-
-# Astra 테마 설치 및 활성화
 wp-cli theme install astra --allow-root
 wp-cli theme update astra --allow-root
 wp-cli theme activate astra --allow-root
 
-# 완료 메시지 출력
 cat <<EOM
 ${GREEN}--------------------
 Wordpress setup has been completed.
@@ -43,5 +31,4 @@ Port: 9000
 --------------------${RESET}
 EOM
 
-# PHP-FPM 서버 시작
 exec /usr/sbin/php-fpm7.3 -F
